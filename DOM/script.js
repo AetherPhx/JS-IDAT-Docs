d4a373e5f3b05bc968cd5289906dacaf72aeac1b
AetherPhx <aetherphx@gmail.com>
2024-08-08 22:16:52 -0500

    agregar nueva versión

--- DOM/script.js
+++ DOM/script.js
@@ -1,7 +1,31 @@
-// TODO: Realizar Menú Hamburguesa
+// * Si los elementos ya existen:
+// const btn = document.querySelector(".hamburger");
+// btn.addEventListener("click", () => {
+// 	const nav = document.querySelector(".nav");
+// 	nav.classList.toggle("hidden");
+// });
 
-const btn = document.querySelector(".hamburger");
+// * Creando los elementos desde JS:
+const app = document.querySelector(".app");
+const btn = document.createElement("button");
+btn.classList.add("hamburger");
+btn.textContent = "Click Me!";
+
+const nav = document.createElement("nav");
+nav.classList.add("nav");
+nav.classList.add("hidden");
+nav.innerHTML = `
+    <ul class="menu">
+        <li><a class="link" href="#">Inicio</a></li>
+        <li><a class="link" href="#">Sobre Nosotros</a></li>
+        <li><a class="link" href="#">Contacto</a></li>
+    </ul>
+`;
+
 btn.addEventListener("click", () => {
 	const nav = document.querySelector(".nav");
 	nav.classList.toggle("hidden");
 });
+
+app.appendChild(btn);
+app.appendChild(nav);
